680651ec48f6ef6b098d1b14092695bbf38c5b6b
tmsski <20782956+tmsski@users.noreply.github.com>
2025-02-10 03:20:41 +0000

    Criar projeto hello

---
+++ hello/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("***********");
+        System.out.println("Bem-vindos!");
+        System.out.println("***********");
+    }
+}
