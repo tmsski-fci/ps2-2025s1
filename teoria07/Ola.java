84c90a326fa97c592da6eb4a4aec6f19d0691d60
tmsasaki <tmsski@yahoo.com.br>
2025-03-31 21:47:13 -0300

    teoria7

---
+++ teoria07/Ola.java
@@ -0,0 +1,6 @@
+class Ola {
+	public static void main(String[] args) {
+		System.out.println("Boa noite!");
+	}
+}
+
---
+++ teoria07/src/main/java/ps2/teoria06/TitularRepo.java
@@ -0,0 +1,7 @@
+package ps2.teoria06;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TitularRepo extends CrudRepository<Titular, Long> {
+
+}
