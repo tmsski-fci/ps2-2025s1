d3e83baf070f002fa559a57a5344224a0ccbfebe
tmsski <20782956+tmsski@users.noreply.github.com>
2025-05-15 00:52:29 +0000

    transações rest client

--- teoria10/src/main/java/ps2/teoria10/FaculdadeController.java
+++ teoria10/src/main/java/ps2/teoria10/FaculdadeController.java
@@ -1,6 +1,5 @@
 package ps2.teoria10;
 
-import org.hibernate.annotations.Generated;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
